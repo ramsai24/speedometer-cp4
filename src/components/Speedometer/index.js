41b42d8dcd8ba1defa555da0d2d84236f3d8f857
ramsai24 <jnrsai24@gmail.com>
2023-05-24 13:30:57 +0000

    all test cases passed

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -5,26 +5,63 @@ import './index.css'
 class Speedometer extends Component {
   state = {speed: 0}
 
-  // onSpeedIncrement() {}
+  onSpeedIncrement = () => {
+    const {speed} = this.state
+    if (speed < 200) {
+      this.setState(preState => {
+        console.log(`${preState.speed}`)
+        return {speed: preState.speed + 10}
+      })
+    } else {
+      this.setState(preState => {
+        console.log(`${preState.speed}`)
+        return {speed: 200}
+      })
+    }
+  }
+
+  onSpeedDecrement = () => {
+    const {speed} = this.state
+    if (speed > 0) {
+      this.setState(preState => {
+        console.log(`${preState.speed}`)
+        return {speed: preState.speed - 10}
+      })
+    }
+  }
 
   render() {
+    const {speed} = this.state
+
     return (
       <div className="speed-container">
         <h1>SPEEDOMETER</h1>
         <img
           src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
-          alt="speedImg"
+          alt="speedometer"
           className="img"
         />
         <h1>Speed is {speed}mph</h1>
-        <p>Min Limit is 0mph,Max limit is 200mph</p>
-
-        <div>
-          <button type="button">Accelrate</button>
-          <button type="button">Apply Brake</button>
+        <p>Min Limit is 0mph, Max Limit is 200mph</p>
+        <div className="btnContainer">
+          <button
+            className="increment"
+            type="button"
+            onClick={this.onSpeedIncrement}
+          >
+            Accelerate
+          </button>
+          <button
+            className="decrement"
+            type="button"
+            onClick={this.onSpeedDecrement}
+          >
+            Apply Brake
+          </button>
         </div>
       </div>
     )
   }
 }
+
 export default Speedometer
