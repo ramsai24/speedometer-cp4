b58c46a8de5ab16ebe8319c0dbde9ed8f4069c6e
ramsai24 <jnrsai24@gmail.com>
2023-05-24 11:40:56 +0000

    error in terminal

---
+++ src/components/Speedometer/index.js
@@ -0,0 +1,29 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class Speedometer extends Component {
+  state = {speed: 0}
+
+  // onSpeedIncrement() {}
+
+  render() {
+    return (
+      <div className="speed-container">
+        <h1>SPEEDOMETER</h1>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+          alt="speedImg"
+          className="img"
+        />
+        <h1>Speed is {speed}mph</h1>
+        <p>Min Limit is 0mph,Max limit is 200mph</p>
+        <div>
+          <button type="button">Accelrate</button>
+          <button type="button">Apply Brake</button>
+        </div>
+      </div>
+    )
+  }
+}
+export default Speedometer
